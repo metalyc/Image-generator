43489bae02e67f17cf05b42606b27414c85f695f
Evan Aprhys <evan.aprhys@gmail.com>
2019-02-10 18:39:53 -0800

    updates to wavetest1.py

--- wavetest1.py
+++ wavetest1.py
@@ -1,14 +1,14 @@
+print("Setting up parameters...")
 from PIL import Image
 from PIL import ImageFilter
 import math
 
-print("Creating image...")
-img = Image.new( 'RGB', (1920, 1080), (250, 250, 250)) # Create a new image (in this case 1920x1080, off-white)
-print("done.")
-print("Creating pixel map...")
+class set:
+    pallet = ((255, 255, 255), (244, 67, 54))
+    size = (1920, 1080)
+
+img = Image.new( 'RGB', set.size, set.pallet[0]) # Create a new image (in this case 1920x1080, off-white)
 pixels = img.load() # Create the pixel map
-print("done.")
-print("Setting parameters...")
 x = int(img.size[0]/2) #parameters for the center of the image
 y = int(img.size[1]/2)
 print("done.")
@@ -18,10 +18,9 @@ for i in range(img.size[0]):    # For every pixel:
     for j in range(img.size[1]):
         for p in range(50): #shadow business creates gradiant, very slow for some reason...
             if j > (math.cos(i/200)*(y-400)) + y + p:
-                q = p + 200
-                pixels[i,-j] = (q, q, q)
+                pixels[i,-j] = (set.pallet[0][0] + p, set.pallet[0][1] + p, set.pallet[0][2] + p)
         if j < (math.cos(i/200)*(y-400))+y: #cosine wave
-            pixels[i,-j-1] = (244, 67, 54)
+            pixels[i,-j-1] = set.pallet[1]
 print("done.")
 
 print("Starting filters...")
@@ -29,6 +28,6 @@ img = img.filter(ImageFilter.SMOOTH_MORE) #filter
 print("done.")
 
 print("Creating file...")
-img.show() #show image
-#img.save("test.png", "PNG") #save image
+#img.show() #show image
+img.save("test.png", "PNG") #save image
 print("done.")
