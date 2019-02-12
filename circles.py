b59c678e2ba21c0319fa56aa65d6e8da914cea64
Bilal <solidhydrogenblock@gmail.com>
2019-02-12 08:57:07 -0800

    added circle thickness variability

--- circles.py
+++ circles.py
@@ -7,6 +7,7 @@ pallet = ((0, 0, 0), (120, 220, 65), (255, 0, 0))
 size = (1920, 1080)
 num_circles = random.randint(1, 10)
 circles = {}
+circle_thickness = 5
 
 for x in range(num_circles):
 	radius = random.randint(0, (size[0] // 4) )
@@ -25,6 +26,11 @@ def print_pixel(x_c, y_c):
 	else:
 		pixels[x_c, y_c] = pallet[0]
 
+def print_block(x_c, y_c, t_t):
+	for x in range(t_t):
+		for y in range(t_t):
+			print_pixel(x_c-1+x, y_c-1+y)
+
 for i in range(img.size[0]):
     for j in range(img.size[1]):
     	c_key = ' '.join(str(e) for e in [i, j])
@@ -33,16 +39,16 @@ for i in range(img.size[0]):
     		for looper in range(r*2):
     			y_c = looper + (j - r)
     			x_c = math.floor(math.sqrt(r**2 - (y_c - j)**2) + i)
-    			print_pixel(x_c, y_c)
+    			print_block(x_c, y_c, circle_thickness)
     			x_c = math.floor(math.sqrt(r**2 - (y_c - j)**2) * -1 + i)
-    			print_pixel(x_c, y_c)
+    			print_block(x_c, y_c, circle_thickness)
 
     		for looper in range(r*2):
     			x_c = looper + (i - r)
     			y_c = math.floor(math.sqrt(r**2 - (x_c - i)**2) + j)
-    			print_pixel(x_c, y_c)
+    			print_block(x_c, y_c, circle_thickness)
     			y_c = math.floor(math.sqrt(r**2 - (x_c - i)**2) * -1 + j)
-    			print_pixel(x_c, y_c)
+    			print_block(x_c, y_c, circle_thickness)
 
 img = img.filter(ImageFilter.SMOOTH_MORE)
 
